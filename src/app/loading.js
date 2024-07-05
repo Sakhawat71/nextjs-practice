3d21f1fe96b0492fc905be3b8f2d35a5419d2dda
sakhawat71 <sakhawat.showrav@gmail.com>
2024-07-05 12:50:52 +0600

    loading page

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+
+
+const LoadingPage = () => {
+
+    return (
+        <div className="flex mx-auto justify-center h-screen items-center">
+            <span className="loading loading-infinity loading-lg"></span>
+        </div>
+    );
+};
+
+export default LoadingPage;
