bea773af06dec2e4d8dbaac4305d2c8548d56de1
sakhawat71 <sakhawat.showrav@gmail.com>
2024-07-05 12:43:30 +0600

    coustom not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='flex mx-auto justify-center min-h-screen items-center'>
+            <div className='font-serif space-y-3 text-center'>
+                <h1 className='text-7xl text-red-600'>404</h1>
+                <p className='text-2xl text-red-700'>Page Not Found !!!</p>
+                <Link className='btn btn-sm' href="/">Go Home</Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
